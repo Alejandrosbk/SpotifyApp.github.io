a639b2a78ab5befad5e4546f4e28f34125928d1d
Alejandrosbk <alejo99sbk@gmail.com>
2021-10-11 18:45:56 -0500

    componentes,modelos, y rutas creadas

---
+++ src/app/shared/shared.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+
+import { SideBarComponent } from '@shared/components/side-bar/side-bar.component';
+import { MediaPlayerComponent } from '@shared/components/media-player/media-player.component';
+import { HeaderUserComponent } from '@shared/components/header-user/header-user.component';
+
+@NgModule({
+  declarations: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent
+  ],
+  imports: [
+    CommonModule
+  ],
+  exports: [
+    SideBarComponent,
+    MediaPlayerComponent,
+    HeaderUserComponent
+  ]
+})
+export class SharedModule { }
